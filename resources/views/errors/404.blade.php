59b220a05b17f6db606783c814ca36b3b805c867
Hidran Arias <hidran@gmail.com>
2024-05-18 13:01:36 +0200

    Adds routes to web.php

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('errors::minimal')
+
+@section('title', __('Not Found'))
+@section('code', '404')
+@section('message')
+    {{$exception->getMessage()}}
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', static fn () => view('welcome'));
+Route::get('users', static function () {
+    //return view('users.index');
+    // return '<h1>Hello world</h1>';
+
+    //    foreach (range(0, 10) as $idx) {
+    //        $user = new stdClass();
+    //        $user->name = 'Hidran'.$idx;
+    //        $user->lastName = 'Arias'.$idx;
+    //        $users[] = $user;
+    //    }
+
+    // return $users;
+    return User::all();
 });
