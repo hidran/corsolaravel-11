bfb594c6dba141920d4f33eef21b035783f887c7
Hidran Arias <hidran@gmail.com>
2024-05-19 18:30:19 +0200

    Adds pages routes and defines then method in app.php withRouting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,12 +3,18 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::prefix('pages')->group(
+                base_path('routes/pages.php')
+            )->middleware('web');
+        }
 
     )
     ->withMiddleware(function (Middleware $middleware) {
