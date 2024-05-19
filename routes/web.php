0de87d72a18ef30733f114bd64f7b8127f885522
Hidran Arias <hidran@gmail.com>
2024-05-19 15:12:00 +0200

    Adds and match optional parameters

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,20 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', static fn() => view('welcome'));
 Route::get('users', static function () {
     return User::all();
 });
-//Route::get('users/{id}', static function (int $id) {
-//    return User::where('id', $id)->first();
-//})
-//    //  ->where(['id' => '[0-9]+']);
-//    //->where('id', '[0-9]+');
-//    ->whereNumber('id');
+
+Route::get('users/{name?}/{email?}/{age?}',
+    static function (?string $name = '', ?string $email = '', ?int $age = 0) {
+        return "$name $email $age";
+    })
+    ->whereAlpha('name')
+    ->whereAlpha('email')
+    ->whereNumber('age');
+
 Route::get('users/{user}', static function (User $user) {
     return $user;
 })
     ->whereNumber('id');
+Route::get('/', static fn() => view('welcome'));
